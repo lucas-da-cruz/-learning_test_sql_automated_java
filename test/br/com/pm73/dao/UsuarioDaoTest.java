b9a5ed5a1578572fed484d8d719dd03966613bd7
Lucas Cruz <CruzLucas@JohnDeere.com>
2020-05-08 09:05:05 -0300

    code app

---
+++ test/br/com/pm73/dao/UsuarioDaoTest.java
@@ -0,0 +1,13 @@
+package br.com.pm73.dao;
+
+import org.hibernate.Session;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class UsuarioDaoTest {
+
+    @Test
+    public void deveEncontrarPeloNomeEEmailMockado(){
+        Session session = Mockito.mock()
+    }
+}
